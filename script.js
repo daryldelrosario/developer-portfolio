3509f7716cdd7cac1d98ce608f7ead0d7f882ff2
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-09-01 11:53:32 -0400

    setup js for maincontent html slideshow

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ setTimeout(() => {
 //   nav.classList.remove("active");
 //   menuIcon.classList.remove("active");
 // }
+
+// SLIDESHOW -------
+let currentImageIndex = 0;
+const images = document.querySelectorAll(".slide");
+
+function switchImage() {
+  images[currentImageIndex].classList.remove("active");
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  images[currentImageIndex].classList.add("active");
+}
+
+setInterval(switchImage, 5000);
